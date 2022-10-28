25be5ce482d09c56c0d22e88ac3cfcc8ac6fa581
siham heddi <siham.heddi.dev@gmail.com>
2022-10-28 22:38:25 +0100

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,60 @@
 
+import { Avatar, Icon,IconButton,TextField } from '@material-ui/core';
 import React from 'react';
 
 
 function NavBar() {
     return ( 
-        <div>
-            {/* search */}
+        <div className='navbar'>
+
+                {/* search */}
+            
+                <div className="search">
+                      {/* <Icon>
+                            search                 
+                      </Icon> */}
+                      <TextField placeholder='Search ...' fullWidth InputProps={{startAdornment: ( 
+                          <Icon> 
+                          search                 
+                      </Icon>)  , disableUnderline: true,} } variant="filled"   />      
+                </div>
+           
             {/* options */}
+
+          
+          <div className="options">
+                <IconButton>
+                   <Icon>
+                      help
+                    </Icon>
+               </IconButton>
+               <div>
+                   <IconButton>
+                        <Icon>
+                            notifications
+                        </Icon>
+                  
+                   </IconButton>
+                   <div className="banner">
+                             3
+                    </div>
+               </div>
+               <div className='info'>
+
+                  <Avatar alt="Remy Sharp" src="https://remysharp.com/images/remy-2017.jpg" />
+                   <div className='content'>
+                       <h4>Ramy Sharp</h4>
+                       <small>lorem ipsum</small>
+                   </div>
+               </div>
+          </div>
+
+
+
+
+
+
+
         </div>
      );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 
+import { Button, Icon } from '@material-ui/core';
 import React from 'react';
 import NavBar from '../components/Navbar';
 import SideBar from '../components/SideBar';
 
+import "../styles/home.css"; 
 
 function Home() {
     return ( 
@@ -11,7 +13,33 @@ function Home() {
             
             <div>
                  <NavBar/>
+                   {/* Main Section */}
+            
+            <div className="main-section">
+                <div className="heading">
+                   <div className="title">
+                        <h2>
+                            Hi Ramy Sharp
+                        </h2>
+                        <small>Welcome To Your Dashboard</small>
+                   </div>
+                  
+                  <Button variant='outlined' color='light'>
+                        <Icon>download</Icon>
+                        Download
+                    </Button>
+                  <Button variant='contained' color='secondary'>
+                        +
+                        Add Dashlet
+                 </Button>
+                </div>
+
             </div>
+            </div>
+
+
+          
+
           
         </div>
      );
