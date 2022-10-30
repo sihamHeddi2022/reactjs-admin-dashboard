31e57a9d459732dae2b07493b3303e39008ce8d5
siham heddi <siham.heddi.dev@gmail.com>
2022-10-30 18:55:08 +0100

    charts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,55 @@
 
-import { Box, Button, Icon, Typography } from '@material-ui/core';
+import { Box, Button, Icon, IconButton, Typography, withStyles } from '@material-ui/core';
 import React from 'react';
 import NavBar from '../components/Navbar';
 import SideBar from '../components/SideBar';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import "../styles/home.css"; 
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 function Home() {
+    const options = {
+        theme: "light",
+        animationEnabled: true,
+        exportFileName: "New Year Resolutions",
+        exportEnabled: true,
+        data: [{
+            type: "pie",
+            showInLegend: true,
+            legendText: "{label}",
+            toolTipContent: "{label}: <strong>{y}%</strong>",
+            indexLabel: "{y}%",
+            indexLabelPlacement: "inside",
+            dataPoints: [
+                { y: 45, label: "send" },
+                { y: 45, label: "open" },
+                { y: 10, label: "spam" },
+           
+            ]
+        }]
+    }
+    const StyledLinearProgress3 = withStyles({
+        colorPrimary: {
+          backgroundColor: "#D0D0D0"
+        },
+        barColorPrimary: {
+          backgroundColor: "green"
+        }
+      })(LinearProgress);
+
+      const StyledLinearProgress4 = withStyles({
+        barColorPrimary: {
+          backgroundColor: "#1ad1ff"
+        },
+        colorPrimary: {
+          backgroundColor: "#e6faff"
+        }
+      })(LinearProgress);
+
+
+
     return ( 
         <div className='content'>
             <SideBar/>
@@ -83,11 +123,11 @@ function Home() {
                            <Box  display="flex" alignItems="center">
 
                            <Box width="100%" mr={1}>
-                           <LinearProgress variant='determinate' color='secondary'   value={60} />                        
+                           <LinearProgress variant='determinate' color='secondary'   value={50} />                        
                            </Box>
 
                               <Box minWidth={35}>
-                                  <Typography variant="body2" color="textSecondary">60%</Typography>
+                                  <Typography variant="body2" color="textSecondary">50%</Typography>
                               </Box>
                            {/* </div> */}
                          </Box>
@@ -109,11 +149,16 @@ function Home() {
                            <Box  display="flex" alignItems="center">
 
                            <Box width="100%" mr={1}>
-                           <LinearProgress variant='determinate' className='infob' value={60} />                        
+                           <StyledLinearProgress3 variant='determinate'   
+                              
+                            
+                              
+                              
+                              value={40} />                        
                            </Box>
 
                               <Box minWidth={35}>
-                                  <Typography variant="body2" color="textSecondary">60%</Typography>
+                                  <Typography variant="body2" color="textSecondary">40%</Typography>
                               </Box>
                            {/* </div> */}
                          </Box>
@@ -123,11 +168,11 @@ function Home() {
                         <CardContent>
                             <div className="details">
                                 <div>
-                                    <h2>2,555.000 $</h2>   
+                                    <h2>778</h2>   
                                     <small>Lorem ipsum dolor sit.</small>   
                                 </div>
-                                <Icon color="primary">
-                                    work
+                                <Icon style={{color:"#1ad1ff"}}>
+                                    cake
                                 </Icon>
                             </div> 
 
@@ -135,11 +180,11 @@ function Home() {
                            <Box  display="flex" alignItems="center">
 
                            <Box width="100%" mr={1}>
-                           <LinearProgress variant='determinate'   value={60} />                        
+                           <StyledLinearProgress4 variant='determinate'   value={80} />                        
                            </Box>
 
                               <Box minWidth={35}>
-                                  <Typography variant="body2" color="textSecondary">60%</Typography>
+                                  <Typography variant="body2" color="textSecondary">80%</Typography>
                               </Box>
                            {/* </div> */}
                          </Box>
@@ -149,6 +194,63 @@ function Home() {
 
 
 
+                <div className="graphs">
+                    <Card >
+                        <CardContent >
+                            <div className='header'>
+                                <div className="title">
+                                    <h2>Email Sent Total</h2>
+                                    <small>March 2020</small>
+                                </div>
+                                <IconButton>
+                                    <Icon>
+                                        more_vert
+                                    </Icon>
+                                </IconButton>
+                            </div>
+              
+
+                        
+                        </CardContent>
+                    </Card>
+                    <Card >
+                        <CardContent className='header'>
+
+                            <div className="title">
+                                 <h2>Income Amounts</h2>
+                               
+                            </div>
+                            <IconButton>
+                                <Icon>
+                                    more_vert
+                                </Icon>
+                            </IconButton>
+                            
+
+                        
+                        </CardContent>
+                    </Card>
+                    <Card >
+                        <CardContent className='header'>
+
+                            <div className="title">
+                                 <h3>Revenue</h3>
+                                 <h2>5690.90 $</h2>
+                                 <small>
+                                      With 20 deals
+                                 </small>
+                            </div>
+                            <IconButton>
+                                <Icon>
+                                    more_vert
+                                </Icon>
+                            </IconButton>
+                            
+
+                        
+                        </CardContent>
+                    </Card>
+                </div>
 
 
 
